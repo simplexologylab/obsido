8374ab332820313bf88475d2b4361339ede93f09
Ramsey Tisher <ramsey@simplexologylab.com>
2021-10-07 06:57:13 -0400

    try this

--- src/components/layout.js
+++ src/components/layout.js
@@ -9,15 +9,15 @@ export default function Layout({ children, headTitle, pageTitle, backLink }) {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main>
-        <div className="flex flex-row bg-black justify-center">
-          <div className="text-white self-center p-2 m-2">
+        <div className="flex flex-row justify-center">
+          <div className="self-center p-2 m-2">
             {backLink && (
               <Link href={backLink}>
                 <a>{`< Back`}</a>
               </Link>
             )}
           </div>
-          <h2 className="flex-auto p-4 text-white text-2xl text-center">
+          <h2 className="flex-auto p-4 text-2xl text-center">
             {pageTitle ? pageTitle : "Obsido"}
           </h2>
         </div>
